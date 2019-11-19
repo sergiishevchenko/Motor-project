5d75c22a83230707a1fe49665b14fdcb968fd7ed
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-11-19 15:12:23 +0100

    fixed DATABASES

--- motor/motor/settings.py
+++ motor/motor/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'motor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'pythondeveloper2019',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
