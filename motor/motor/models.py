8a6ee5ea339b21b5d179abc08ad70ed9d159e158
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-11-20 21:50:56 +0100

    new migrations

--- motor/motor/models.py
+++ motor/motor/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from datetime import date
-import datetime
 
 
 class User(models.Model):
@@ -15,69 +13,29 @@ class User(models.Model):
         ('Ж', 'Женский')
     ]
 
-    Login = models.CharField(
-        max_length=30,
-        verbose_name='Логин пользователя'
-    )
-    
-    FirstName = models.CharField(
-        max_length=30,
-        verbose_name='Имя пользователя'
-    )
-    
-    LastName = models.CharField(
-        max_length=30,
-        verbose_name='Фамилия пользователя'
-    )
-    
-    Gender = models.CharField(
-        max_length=1,
-        choices=GENDER_LIST,
-        verbose_name='Пол пользователя'
-    )
-    
-    Phone = models.CharField(
-        max_length=30,
-        verbose_name='Мобильный телефон пользователя'
-    )
-    
-    Email = models.CharField(
-        max_length=30,
-        verbose_name='Емейл пользователя',
-        unique=True
-    )
+    Login = models.CharField(max_length=30, verbose_name='Логин пользователя')
+    FirstName = models.CharField(max_length=30, verbose_name='Имя пользователя')
+    LastName = models.CharField(max_length=30, verbose_name='Фамилия пользователя')
+    Gender = models.CharField(max_length=1, choices=GENDER_LIST, verbose_name='Пол пользователя')
+    Phone = models.CharField(max_length=30, verbose_name='Мобильный телефон пользователя')
+    Email = models.CharField(max_length=30, verbose_name='Емейл пользователя', unique=True)
+    Password = models.CharField(max_length=30, verbose_name='Пароль пользователя', default='None')
+    CreatedDate = models.DateTimeField(verbose_name='Дата создания', help_text='День, когда был создан аккаунт', auto_now_add=True)
+    ModifiedDate = models.DateTimeField(verbose_name='Дата последнего изменения', help_text='Когда было изменено что-нибудь в аккаунте', auto_now_add=True)
+    Active = models.BooleanField(verbose_name='Статус пользователя', help_text='Активен / Не активен', default=True)
+    GroupID = models.ForeignKey('UserGroup', on_delete=models.SET_NULL, verbose_name='Группа пользователя', help_text='К какой группе относится пользователь', null=True)
 
-    Password = models.CharField(
-        max_length=30,
-        verbose_name='Пароль пользователя',
-        default='None'
-    )
-    
-    CreatedDate = models.DateTimeField(
-        verbose_name='Дата создания',
-        help_text='День, когда был создан аккаунт',
-        auto_now_add=True
-    )
-    
-    ModifiedDate = models.DateTimeField(
-        verbose_name='Дата последнего изменения',
-        help_text='Когда было изменено что-нибудь в аккаунте',
-        auto_now_add=True
-    )
-        
-    Active = models.BooleanField(
-        verbose_name='Статус пользователя',
-        help_text='Активен / Не активен',
-        default=True
-    )
 
-    GroupID = models.ForeignKey(
-        'UserGroup',
-        on_delete=models.SET_NULL,
-        verbose_name='Группа пользователя',
-        help_text='К какой группе относится пользователь',
-        null=True        
-    )
+class UserGroup(models.Model):
+    class Meta:
+        verbose_name_plural = 'Пользовательские группы'
+
+    def __str__(self):
+        return f'Группа {self.GroupType}'
+
+    GroupType = models.CharField(max_length=15, verbose_name='Группа пользователя')
+    Active = models.BooleanField(verbose_name='Статус группы', help_text='Активена / Не активена')
+
 
 """
 class LoginAttempt(models.Model):
@@ -97,22 +55,3 @@ class Password(models.Model):
     PasswordQuestion
     PasswordAnswer
 """
-
-class UserGroup(models.Model):
-    class Meta:
-        verbose_name_plural = 'Пользовательские группы'
-
-    def __str__(self):
-        return f'Группа {self.GroupType}'
-
-    GroupType = models.CharField(
-        max_length=15,
-        verbose_name='Группа пользователя',
-    )
-
-    Active = models.BooleanField(
-        verbose_name='Статус группы',
-        help_text='Активена / Не активена'
-    )
-
-
