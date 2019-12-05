052972f44a75e905df403c3fff097dcebe29e40f
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-12-05 20:50:20 +0100

    added host

--- motor/settings.py
+++ motor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = 'qox++ud@j*6m5s!pgq7!gp0f6k#bfn9bsjyq#hd8kcbklci_i%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
