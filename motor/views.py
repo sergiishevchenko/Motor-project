999394b73119ba5b45e7455e0c83de801c7b1e79
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-02-12 20:09:18 +0100

    comparisons carousel

--- motor/views.py
+++ motor/views.py
@@ -126,7 +126,7 @@ def index(request):
                     'model_series_sum': model_series_sum,
                     'models_infinity': models_infinity,
                     'models_cars': models_cars}
-    return render(request, 'motor/index.html', params)
+    return render(request, 'motor/comparison.html', params)
 
 
 def auto(request):
